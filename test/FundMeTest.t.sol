93ce53641963df6a2869bfb275bf9004e88c0423
ayberklevendoglu <ayberklevendoglu@gmail.com>
2024-06-27 01:50:03 +0300

    add helper config and stuff

--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -1,13 +1,17 @@
+// SPDX-License-Identifier: MIT
+
 pragma solidity ^0.8.18;
 
 import {Test, console} from "forge-std/Test.sol";
 import {FundMe} from "../src/FundMe.sol";
+import {DeployFundMe} from "../script/DeployFundMe.s.sol";
 
 contract testFundMe is Test {
     FundMe fundMe;
 
     function setUp() external {
-        fundMe = new FundMe();
+        DeployFundMe deployFundMe = new DeployFundMe();
+        fundMe = deployFundMe.run();
     }
 
     function testMinimumDollarIsFive() public view {
@@ -16,7 +20,7 @@ contract testFundMe is Test {
     }
 
     function testOwnerIsMsgSender() public view {
-        assertEq(fundMe.i_owner(), address(this));
+        assertEq(fundMe.i_owner(), msg.sender);
     }
 
     function testPriceFeedVersion() public view {
